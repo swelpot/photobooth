4aaf7dc11926e3a49e3c74a46d8534c310e9212e
stefan <stefan@welpot.de>
2017-08-24 22:13:43 +0200

    bugfix

--- PhotoboothApp.py
+++ PhotoboothApp.py
@@ -47,7 +47,7 @@ class ButtonPressedScreen(Screen):
 
     def init_video(self, video_files):
         Logger.debug("ButtonPressedScreen.init_video()")
-        self.video_buttonpressed.source = video_files
+        #self.video_buttonpressed.source = video_files
 
     def play(self):
         Logger.debug("ButtonPressedScreen.play()")
@@ -80,7 +80,7 @@ class MainApp(App):
     def build(self):
         Logger.debug("MainApp.build()")
         Clock.schedule_interval(self.inner_button_pressed, 0.1)
-        Clock.schedule_interval(self.inner_show_image, 0.5)
+        Clock.schedule_interval(self.inner_show_image, 5)
 
         self.scr_loop_video.init_video(conf.get("app.video_loop"))
         self.scr_button_pressed.init_video(conf.get("app.video_buttonpressed"))
@@ -108,7 +108,7 @@ class MainApp(App):
             self.scr_image.set_image(self.image_path)
             self.sm.current = 'show_image'
 
-            Clock.schedule_once(self.inner_show_loop_video, conf.get("app.image_show_duration"))
+            Clock.schedule_once(self.inner_show_loop_video, conf.get("app.show_image_duration"))
 
     def inner_show_loop_video(self, *args):
         Logger.debug("MainApp.show_loop_video()")
