4a27fa397ea4b4bbe9d268c1deb46c81f4493a1b
stefan <stefan@welpot.de>
2017-08-29 21:53:45 +0200

    button integration

--- ButtonController.py
+++ ButtonController.py
@@ -19,6 +19,7 @@ class ButtonController(Thread):
     def run(self):
         while True:
             input_state = GPIO.input(gpioPin)
+            Logger.debug("state: {0}".format(input_state))
             if input_state == False:
                 self.button_pressed()
                 time.sleep(0.2)
