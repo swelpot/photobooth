34b1a06bff7a3079ba126b5d3185e812c953a61b
stefan <stefan@welpot.de>
2017-09-23 22:43:58 +0200

    trial & error

--- controller/Camera4ControllerDummy.py
+++ controller/Camera4ControllerDummy.py
@@ -6,10 +6,10 @@ class Camera4Controller(object):
 
     def shoot(self):
         Logger.debug("Camera4ControllerDummy.shoot()")
-        return ["../photos/org/_MG_6161.JPG",
-                "../photos/org/_MG_6162.JPG",
-                "../photos/org/_MG_6163.JPG",
-                "../photos/org/_MG_6164.JPG"]
+        return ["../photos/resized/_MG_6161.JPG",
+                "../photos/resized/_MG_6162.JPG",
+                "../photos/resized/_MG_6163.JPG",
+                "../photos/resized/_MG_6164.JPG"]
 
     def initCamera(self):
         pass
