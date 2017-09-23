b0db8c34d422e2830ad1a6d02565ec71be5f1612
stefan <stefan@welpot.de>
2017-09-24 00:10:44 +0200

    trial & error

--- screens/ShowImageScreen.py
+++ screens/ShowImageScreen.py
@@ -14,7 +14,6 @@ class ShowImageScreen(Screen):
     r = NumericProperty(0.0)
     g = NumericProperty(0.0)
     b = NumericProperty(0.0)
-    a = NumericProperty(0.5)
 
     pos_x = NumericProperty()
 
@@ -33,17 +32,18 @@ class ShowImageScreen(Screen):
         Logger.debug("ShowImageScreen.set_image() with {0}".format(image))
         self.obj_image.source = image
 
-        if self.controller.get_conf("app.printing_enabled"):
+        #if self.controller.get_conf("app.printing_enabled"):
             # show print dialog after x seconds if printing enabled
-            self.event = Clock.schedule_once(self._show_print_dialog, self.controller.get_conf("app.show_image_duration"))
+            #self.event = Clock.schedule_once(self._show_print_dialog, self.controller.get_conf("app.show_image_duration"))
 
     def on_touch_up(self, touch):
         Logger.debug("Touch UP: x: {0}, y: {1}".format(touch.px, touch.py))
 
         # show print dialog when screen pressed
         if self.controller.get_conf("app.printing_enabled"):
-            self.event.cancel()
-            self._show_print_dialog()
+            #self.event.cancel()
+            #self._show_print_dialog()
+            self.abort_print_dialog()
 
     def print_image(self):
         """ called from kv file """
@@ -55,7 +55,7 @@ class ShowImageScreen(Screen):
     def abort_print_dialog(self):
         """ called from kv file """
         self.controller.show_loop_screen()
-        self._hide_print_dialog()
+        #self._hide_print_dialog()
 
     def _show_print_dialog(self, *args):
         Logger.debug('ShowImageScreen.show_print_dialog()')
@@ -68,4 +68,5 @@ class ShowImageScreen(Screen):
         self.pos_x = 500
         self.obj_lbl_print_copies.text = '1'
 
+#Builder.load_file("screens/ShowImagePrintDialog.kv")
 Builder.load_file("screens/ShowImageScreen.kv")
