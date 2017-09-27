36a69c9297bba9fb909d9dbf818a5a93d7972f71
stefan <stefan@welpot.de>
2017-09-27 19:45:27 +0200

    7 Segment Display

--- controller/SegmentDisplayController.py
+++ controller/SegmentDisplayController.py
@@ -4,33 +4,34 @@ from threading import Thread
 import time
 import datetime
 
-IS_MAC = False
+IS_RPI = True
 try:
     from Adafruit_LED_Backpack import SevenSegment
 except:
-    IS_MAC = True
-
+    IS_RPI = False
 
 MODE_COUNTDOWN_TRIGGER = 1
 MODE_COUNTDOWN_PHOTO = 2
 MODE_LOOP = 3
 
+MULTIPLIER_MICRO = 1000000.0
+
+
 class SegmentDisplayController(Thread):
     _display = None
 
-    _seconds = 10 # initial
-    _init_seconds = 3.0 # initial
-    _photo_number = 1 # initial
-
-    _counter = 0
+    _seconds = 10  # initial
+    _init_seconds = 3.0  # initial
+    _photo_number = 1  # initial
 
     _current_mode = MODE_LOOP
+    _start_time = datetime.datetime.now()
 
     def __init__(self):
         super(SegmentDisplayController, self).__init__()
         self.daemon = True
 
-        if not IS_MAC:
+        if IS_RPI:
             # Create display instance on default I2C address (0x70) and bus number.
             self._display = SevenSegment.SevenSegment()
 
@@ -45,99 +46,138 @@ class SegmentDisplayController(Thread):
                 self._display = None
 
     def run(self):
-        sleep_time = 0.25
-        next_clear = False
+        sleep_time = 0.05
+        frequency = 2  # per second, default
+
+        digits = [None] * 4
+        colon = False  # show colon
+        visible = True  # number is visible
 
         while True:
+            now = datetime.datetime.now()
+            diff = (now - self._start_time).microseconds
+            div = 1.0 / frequency * MULTIPLIER_MICRO
+            rest = diff % div
+            visible = False if rest > 0 and rest < (sleep_time * MULTIPLIER_MICRO) else True
+
+            # print "Now {0}, Start {1}, diff {2}, div {3}, rest {4}, io {5}".format(now, self._start_time, diff, div, rest, io)
+
             if self._current_mode == MODE_COUNTDOWN_TRIGGER:
-                self._counter = self._counter + 1
-                #sleep_time = 0.25
+                frequency = 2  # per second
+                number = int(self._seconds + self._init_seconds - (now - self._start_time).seconds)
+                if number < 0:
+                    number = 0
+                elif number > self._seconds:
+                    number = self._seconds
 
-                if self._init_seconds >= 0:
-                    self.show_number(self._seconds, False, next_clear)
-                    self._init_seconds = self._init_seconds - sleep_time
-                else:
-                    self.show_number(self._seconds, False, next_clear)
+                digits = self._convert(number)
+                colon = False
 
-                    if self._counter > (1.0 / sleep_time) and self._seconds >= 0:
-                        self._counter = 0
-                        self._seconds = self._seconds - 1
-
-                    elif self._counter > (1.0 / sleep_time) and self._seconds < 0:
-                        self._current_mode = MODE_COUNTDOWN_PHOTO
+                if number == 1:
+                    # fast blink last second
+                    frequency = 5
+                elif number == 0:
+                    frequency = 10
 
             elif self._current_mode == MODE_COUNTDOWN_PHOTO:
-                #sleep_time = 0.25
-                self.show_number(self._photo_number, False, next_clear)
+                frequency = 2
+                digits = self._convert(self._photo_number)
+                colon = False
+
             else:
-                now = datetime.datetime.now()
+                frequency = 2
+
                 hour = now.hour
                 minute = now.minute
                 second = now.second
 
                 number = hour * 100 + minute
+                digits = self._convert(number)
                 colon = second % 2
+                visible = True
 
-                self.show_number(number, colon, False)
+            self.show_number(digits, colon, visible)
 
-            # do loop
-            next_clear = not next_clear
             time.sleep(sleep_time)
 
-    def run_countdown_trigger(self, seconds, init_seconds, trigger_count):
-        self._seconds = seconds
-        self._init_seconds = float(init_seconds)
-        self._photo_number = trigger_count
-        self._counter = 0
+    def _convert(self, number):
+        #print "number: " + str(number)
+        digit1 = number / 1000
+        digit2 = (number - digit1 * 1000) / 100
+        digit3 = (number - digit1 * 1000 - digit2 * 100) / 10
+        digit4 = number - digit1 * 1000 - digit2 * 100 - digit3 * 10
 
-        self._current_mode = MODE_COUNTDOWN_TRIGGER
+        if digit1 == 0:
+            digit1 = None
 
-    def run_countdown_photo(self, photo_number):
-        self._photo_number = photo_number
+            if digit2 == 0:
+                digit2 = None
 
-        self._current_mode = MODE_COUNTDOWN_PHOTO
+                if digit3 == 0: digit3 = None
+        #if digit4 == 0: digit4 = None
 
-    def run_loop(self):
-        self._current_mode = MODE_LOOP
+        return [digit1, digit2, digit3, digit4]
 
-    def show_number(self, number, colon, clear_display):
+    _current_digits = [None] * 4
+    _current_colon = False
+    _current_visible = True
+
+    def show_number(self, digits, colon, visible):
+
+        if self._current_digits == digits and self._current_colon == colon and self._current_visible == visible:
+            return
+
+        self._current_digits = digits
+        self._current_colon = colon
+        self._current_visible = visible
+
         if self._display:
             # Clear the display buffer.
             self._display.clear()
 
-            if not clear_display:
-                pos1 = number // 1000
-                pos2 = (number - pos1) // 100
-                pos3 = (number - pos1 - pos2) // 10
-                pos4 = number - pos1 - pos2 - pos3
-                self._display.set_digit(0, pos1)
-                self._display.set_digit(1, pos2)
-                self._display.set_digit(2, pos3)
-                self._display.set_digit(3, pos4)
-                # Print a floating point number to the display.
-                #self._display.print_float(number, decimal_digits=0)
+            if visible:
+                self._display.set_digit(0, digits[0])
+                self._display.set_digit(1, digits[1])
+                self._display.set_digit(2, digits[2])
+                self._display.set_digit(3, digits[3])
+
                 # Set the colon on or off (True/False).
                 self._display.set_colon(colon)
 
             # Write the display buffer to the hardware.  This must be called to
             # update the actual display LEDs.
             self._display.write_display()
-        elif IS_MAC:
+        elif not IS_RPI:
             now = datetime.datetime.now()
 
-            if not clear_display:
-                    logging.error(str(now) + " " + str(number) + (":" if colon else ""))
+            if visible:
+                print str(now) + " " + str(digits[0]) + "," + str(digits[1]) + "," + (":" if colon else "") + str(digits[2]) + "," + str(
+                    digits[3])
             else:
-                logging.info(str(now) + " clear")
+                print str(now)
+
+
+    def run_countdown_trigger(self, seconds, init_seconds):
+        self._start_time = datetime.datetime.now()
+        self._seconds = seconds
+        self._init_seconds = init_seconds
+        self._current_mode = MODE_COUNTDOWN_TRIGGER
+
+
+    def run_countdown_photo(self, photo_number):
+        self._start_time = datetime.datetime.now()
+        self._photo_number = photo_number
+        self._current_mode = MODE_COUNTDOWN_PHOTO
+
 
 if __name__ == '__main__':
-
     d = SegmentDisplayController()
     d.start()
 
     time.sleep(10)
-    d.run_countdown_trigger(10, 3, 4)
-    time.sleep(15)
+    seconds = 10
+    d.run_countdown_trigger(seconds, 1)
+    time.sleep(seconds + 3)
     d.run_countdown_photo(4)
     time.sleep(2)
     d.run_countdown_photo(3)
@@ -148,4 +188,3 @@ if __name__ == '__main__':
     time.sleep(2)
     d.run_countdown_photo(0)
     time.sleep(2)
-    d.show_number(1, False, True)
