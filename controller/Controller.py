b504ca3f2bfca028d5680924ae0cdafc99c5ba9d
stefan <stefan@welpot.de>
2017-10-06 01:11:30 +0200

    reverted dummy

--- controller/Controller.py
+++ controller/Controller.py
@@ -8,7 +8,7 @@ from kivy.logger import Logger
 
 from ButtonController import ButtonController
 from SegmentDisplayController import SegmentDisplayController
-from controller.Camera4ControllerDummy import Camera4Controller
+from controller.Camera4Controller import Camera4Controller
 from util.Collage4Creator import Collage4Creator
 from util.ConfUtil import ConfUtil
 from util.FileUtil import FileUtil
